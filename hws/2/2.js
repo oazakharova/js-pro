7a68b4bbbade8663b0fe0d8c994fb769779f3f29
Olga <o.nikolaichenko@ya.ru>
2024-04-10 18:08:01 +0200

    task 2 added

--- hws/2/2.js
+++ hws/2/2.js
@@ -65,3 +65,84 @@ const initialData = [
     ],
   },
 ];
+
+const productsElements = document.querySelectorAll(".product");
+
+// вывод на страницу дефолтных отзывов
+productsElements.forEach((productInHTML) => {
+  initialData.forEach((initialDataEl) => {
+    if (
+      productInHTML.querySelector(".product-name").querySelector("a")
+        .textContent === initialDataEl.product
+    ) {
+      initialDataEl.reviews.forEach((reviewEl) => {
+        const feedbacksWrapEl = productInHTML.querySelector(".feedbacks-wrap");
+
+        const feedbackEl = document.createElement("div");
+        feedbackEl.classList.add("feedback");
+        feedbackEl.innerHTML = `
+          <div class="feedback-text">
+            ${reviewEl.text}
+          </div>
+        `;
+
+        feedbacksWrapEl.append(feedbackEl);
+      });
+    }
+  });
+});
+
+// textarea
+productsElements.forEach((productInHTML) => {
+  const textareaEl = productInHTML.querySelector("textarea");
+  const buttonEl = productInHTML.querySelector("button");
+
+  buttonEl.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    initialData.forEach((initialDataEl) => {
+      if (
+        productInHTML.querySelector(".product-name").querySelector("a")
+          .textContent === initialDataEl.product
+      ) {
+        // проверка длины отзыва
+        if (textareaEl.value.length < 50 || textareaEl.value.length > 500) {
+          7;
+          const textareaErrorEl =
+            productInHTML.querySelector(".textarea-error");
+          textareaErrorEl.textContent =
+            "Длина введенного отзыва не соответствует требованиям";
+          throw new Error(
+            "Длина введенного отзыва не соответствует требованиям"
+          );
+        }
+
+        // добавление нового отзыва в initialDataEl
+        initialDataEl.reviews.push({
+          id: uid(),
+          text: textareaEl.value,
+        });
+
+        // добавление нового отзыва в initialDataEl
+        initialDataEl.reviews.push({
+          id: uid(),
+          text: textareaEl.value,
+        });
+
+        // отображение нового отзыва на странице
+        const feedbacksWrapEl = productInHTML.querySelector(".feedbacks-wrap");
+        const feedbackEl = document.createElement("div");
+        feedbackEl.classList.add("feedback");
+        feedbackEl.innerHTML = `
+          <div class="feedback-text">
+            ${textareaEl.value}
+          </div>
+        `;
+        feedbacksWrapEl.append(feedbackEl);
+
+        // очистка поля textarea
+        textareaEl.value = "";
+      }
+    });
+  });
+});
