fc0236ba4c6ddc11419182c57dc7d78961242542
Olga <o.nikolaichenko@ya.ru>
2024-04-10 14:59:22 +0200

    task 1 added

--- hws/2/1.js
+++ hws/2/1.js
@@ -18,3 +18,78 @@
 библиотеке и возвращать true или false в зависимости от того, есть ли такая
 книга в списке или нет.
 */
+
+class Library {
+  #books = [];
+
+  constructor(books) {
+    if (!books?.length) {
+      throw new Error("Передан пустой массив");
+    }
+    if (books?.length !== new Set(books).size) {
+      throw new Error("В предоставленном массиве содержатся дубликаты");
+    }
+    this.#books.push(...books);
+  }
+
+  getAllBooks() {
+    return this.#books;
+  }
+
+  addBook(title) {
+    if (this.#books?.includes(title)) {
+      throw new Error("Книга с таким названием уже есть в списке.");
+    } else this.#books.push(title);
+  }
+
+  hasBook(title) {
+    const indexOfTitle = this.#books?.indexOf(title);
+    if (indexOfTitle !== -1) {
+      return true;
+    }
+    return false;
+  }
+
+  removeBook(title) {
+    const indexOfTitle = this.#books.indexOf(title);
+    if (indexOfTitle !== -1) {
+      this.#books.splice(indexOfTitle, 1);
+    } else {
+      throw new Error(
+        "Книги с таким названием нет в списке, удаление невозможно"
+      );
+    }
+  }
+}
+
+// массивы книг
+// const myBooksWithDuplicate = [
+//   "Anna Karenina",
+//   "Anna Karenina",
+//   "The Great Gatsby",
+//   "Hamlet",
+//   "One Hundred Years of Solitude",
+//   "Crime and Punishment",
+// ];
+const myBooks = [
+  "Anna Karenina",
+  "The Great Gatsby",
+  "Hamlet",
+  "One Hundred Years of Solitude",
+  "Crime and Punishment",
+];
+
+// const library1 = new Library(myBooksWithDuplicate);
+
+const library2 = new Library(myBooks);
+console.log(library2.getAllBooks());
+library2.addBook("Война и мир");
+console.log(library2.getAllBooks());
+library2.addBook("Винни Пух");
+console.log(library2.getAllBooks());
+library2.removeBook("Винни Пух");
+console.log(library2.getAllBooks());
+console.log(library2.hasBook("Война и мир"));
+console.log(library2.hasBook("Алиса в стране чудес"));
+
+// const library3 = new Library();
